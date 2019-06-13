3262515198d7030ff75681eab4093b357af2d9f9
hanjianjie <hanjj@firstgrid.cn>
2019-06-13 10:23:57 +0800

    node require 模块缓存的验证

--- node/require/demo01.js
+++ node/require/demo01.js
@@ -1,7 +1,18 @@
-console.log('module.id: ', module.id);
-console.log('module.exports: ', module.exports);
-console.log('module.parent: ', module.parent);
-console.log('module.filename: ', module.filename);
-console.log('module.loaded: ', module.loaded);
-console.log('module.children: ', module.children);
-console.log('module.paths: ', module.paths);
+// console.log('module.id: ', module.id);
+// console.log('module.exports: ', module.exports);
+// console.log('module.parent: ', module.parent);
+// console.log('module.filename: ', module.filename);
+// console.log('module.loaded: ', module.loaded);
+// console.log('module.children: ', module.children);
+// console.log('module.paths: ', module.paths);
+
+
+const _ = require('lodash');
+
+_.specail = () => {
+  console.log('this is a specail func');
+};
+
+_.specail();
+
+// console.log(require.cache);
--- node/require/demo02.js
+++ node/require/demo02.js
@@ -1,1 +1,17 @@
 const d = require('./demo01');
+
+delete require.cache[require.resolve('lodash')];
+
+console.table(require.cache);
+
+
+console.log('-------------------------------');
+
+const _ = require('lodash');
+
+
+
+
+// console.log('----------------------');
+
+console.log(_.specail());
